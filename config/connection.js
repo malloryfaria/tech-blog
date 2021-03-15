cc2d671d088a219561b614591bed5b4f81ba0b6b
Mallory <mallory.faria@gmail.com>
2021-03-14 21:22:51 -0400

    updated port

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+const PORT = process.env.PORT || 3001;
 
 require('dotenv').config();
 
@@ -10,7 +11,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
-    port: 80
+    port: PORT
   });
 }
 
